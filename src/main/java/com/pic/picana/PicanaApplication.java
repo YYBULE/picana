5e180b38fdb8773c549f20e52046003f62b6f02c
Yan, Yi <yany0630@gmail.com>
2018-04-19 17:33:00 +0800

    Add first version

---
+++ src/main/java/com/pic/picana/PicanaApplication.java
@@ -0,0 +1,12 @@
+package com.pic.picana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicanaApplication.class, args);
+	}
+}
